7196d1688b9d998453e8e7232826fbb72a9acd02
jeisonOrtega <jeisonempleo1@gmail.com>
2022-10-22 11:30:04 -0300

    Creacion de  App y definicion de rutas

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const { Router } = require("express")
+const express = require ("express")
+const app = express()
+
+app.use(express.json())
+
+const router = Router()
+
+router.get("/suma")
+
+router.get("/resta")
+
+router.get("/multiplicaion")
+
+router.get("/division")
+
+app.listen(3300,()=>console.log("servidor rodando"))
